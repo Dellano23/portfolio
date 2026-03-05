26040e234962ab178c47dd1711aba6d99a40444d
Dellano <124741424+Dellano23@users.noreply.github.com>
2026-03-05 17:57:31 +0000

    add `.database.js` and `.env` files

--- portfolio-next/app/api/v1/status/route.js
+++ portfolio-next/app/api/v1/status/route.js
@@ -1,6 +1,10 @@
+import database from "../../../../infra/database.js";
+
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const result = await database.query("SELECT 1 + 1 AS sum;"); //sql sum test
+  console.log(result.rows);
   return NextResponse.json({
     chave: "Teste api next",
     status: "ok",
